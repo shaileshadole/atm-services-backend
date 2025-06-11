cf9ef6e1ece4eb8a1b4dbfff2047bacf9a66c658
shaileshadole <159097351+shaileshadole@users.noreply.github.com>
2025-06-11 11:25:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,14 +12,16 @@ config({
     path: "./data/config.env",
 })
 
-//Using middlewares
-app.use(express.json());
-app.use(cookieParser());
 app.use(cors({
     origin: [process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     credentials: true, //permission to include user credentials such as cookies and authentication headers
 }));
+
+//Using middlewares
+app.use(express.json());
+app.use(cookieParser());
+
 
 //Using routes after express json
 app.use("/api/v1/users", userRouter);
